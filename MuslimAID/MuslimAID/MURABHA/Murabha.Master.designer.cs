9efa5e16103974d0a267bf633a939b70c9e6cd30
Thilanka <thilanka@proitsolutions.lk>
2016-06-12 12:44:06 +0530

    Maaster

--- MuslimAID/MuslimAID/MURABHA/Murabha.Master.designer.cs
+++ MuslimAID/MuslimAID/MURABHA/Murabha.Master.designer.cs
@@ -23,13 +23,13 @@ namespace MuslimAID.MURABHA {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
         
         /// <summary>
-        /// form1 control.
+        /// frmMurabha control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        protected global::System.Web.UI.HtmlControls.HtmlForm frmMurabha;
         
         /// <summary>
         /// ContentPlaceHolder1 control.
