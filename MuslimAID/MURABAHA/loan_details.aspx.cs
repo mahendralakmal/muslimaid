752133cef55b447172fa94c06a8ea00f85427421
mahen <gitlab.proit@gmail.com>
2016-12-21 18:40:11 +0800

    Added loandetails form

--- MuslimAID/MURABAHA/loan_details.aspx.cs
+++ MuslimAID/MURABAHA/loan_details.aspx.cs
@@ -72,7 +72,7 @@ namespace MuslimAID.MURABHA
 
         protected void btnSubmit_Click(object sender, EventArgs e)
         {
-            string q = "INSERT INTO `muslimaid`.`micro_loan_details`(`contra_code`,`loan_amount`,`current_loan_amount`,`service_charges`,`other_charges`,`interest_rate`,`interest_amount`,`period`,`monthly_instollment`,`created_on`,`created_user_nic`,`created_user_ip`,`chequ_no`,`chequ_amount`,`chequ_deta_on`,`loan_approved`,`loan_approved_user_nic`,`loan_approved_on`,`OtherDescription`,`cheq_detai_app_nic`,`due_date`,`arres_amou`,`acc_name`,`acc_branch`,`acc_number`,`bank_name`,`def`,`over_payment`,`arres_count`,`loan_sta`,`ser_char_sta`,`closing_date`,`maturity_date`,`due_installment`,`reg_approval_nic`,`reg_approval_on`,`reg_approval_des`,`reg_approval`,`bank_code`,`branch_code`,`registration_fee`,`walfare_fee`)VALUES(`@contra_code`,`@loan_amount`,`@current_loan_amount`,`@service_charges`,`@other_charges`,`@interest_rate`,`@interest_amount`,`@period`,`@monthly_instollment`,`@created_on`,`@created_user_nic`,`@created_user_ip`,`@chequ_no`,`@chequ_amount`,`@chequ_deta_on`,`@loan_approved`,`@loan_approved_user_nic`,`@loan_approved_on`,`@OtherDescription`,`@cheq_detai_app_nic`,`@due_date`,`@arres_amou`,`@acc_name`,`@acc_branch`,`@acc_number`,`@bank_name`,`@def`,`@over_payment`,`@arres_count`,`@loan_sta`,`@ser_char_sta`,`@closing_date`,`@maturity_date`,`@due_installment`,`@reg_approval_nic`,`@reg_approval_on`,`@reg_approval_des`,`@reg_approval`,`@bank_code`,`@branch_code`,`@registration_fee`,`@walfare_fee`)";
+            string q = "INSERT INTO `muslimaid`.`micro_loan_details`(contract_code,loan_amount,product_category,brand,model_no,selling_price,down_payment,service_charges,registration_fee,walfare_fee,other_charges,interest_rate,period,monthly_instollment,reason_to_apply,any_unsettled_loans,other_unsettled_facilities`) VALUES (@contract_code,@loan_amount,@product_category,@brand,@model_no,@selling_price,@down_payment,@service_charges,@registration_fee,@walfare_fee,@other_charges,@interest_rate,@period,@monthly_instollment,@reason_to_apply,@any_unsettled_loans,@other_unsettled_facilities)";
 
             try
             {
@@ -80,10 +80,36 @@ namespace MuslimAID.MURABHA
                     lblMsg.Text = "Contract Code cannot be empty";
                 else if (txtLDIntRate.Text.Trim() == "")
                     lblMsg.Text = "Please enter Facility Amount/ Value";
+                else if (txtNameOrg1.Text.Trim() != "")
+                {
+                    if (txtPurpos1.Text.Trim() == "") lblMsg.Text = "Please fill the field purpose 1";
+                    if (txtFAmount1.Text.Trim() == "") lblMsg.Text = "Please fill the field facility amount 1";
+                    if (txtOutstandBal1.Text.Trim() == "") lblMsg.Text = "Please fill the field outstanding balance 1";
+                    if (txtMonthInstal1.Text.Trim() == "") lblMsg.Text = "Please fill the field monthly installment 1";
+                    if (txtRemainInstal1.Text.Trim() == "") lblMsg.Text = "Please fill the field remaining no of installment 1";
+
+                    else if (txtNameOrg2.Text.Trim() != "")
+                    {
+                        if (txtPurpos2.Text.Trim() == "") lblMsg.Text = "Please fill the field purpose 2";
+                        if (txtFAmount2.Text.Trim() == "") lblMsg.Text = "Please fill the field facility amount 2";
+                        if (txtOutstandBal2.Text.Trim() == "") lblMsg.Text = "Please fill the field outstanding balance 2";
+                        if (txtMonthInstal2.Text.Trim() == "") lblMsg.Text = "Please fill the field monthly installment 2";
+                        if (txtRemainInstal2.Text.Trim() == "") lblMsg.Text = "Please fill the field remaining no of installment 1";
+                    }
+
+                    else if (txtNameOrg3.Text.Trim() != "")
+                    {
+                        if (txtPurpos3.Text.Trim() == "") lblMsg.Text = "Please fill the field purpose 3";
+                        if (txtFAmount3.Text.Trim() == "") lblMsg.Text = "Please fill the field facility amount 3";
+                        if (txtOutstandBal3.Text.Trim() == "") lblMsg.Text = "Please fill the field outstanding balance 3";
+                        if (txtMonthInstal3.Text.Trim() == "") lblMsg.Text = "Please fill the field monthly installment 3";
+                        if (txtRemainInstal3.Text.Trim() == "") lblMsg.Text = "Please fill the field remaining no of installment 3";
+                    }
+                }
+                
                 else
                 {
                     MySqlCommand cmdInsert = new MySqlCommand(q);
-
                     #region Parameter Declarations
                     cmdInsert.Parameters.AddWithValue("`@contract_code`", txtCC.Text.Trim());
                     cmdInsert.Parameters.AddWithValue("`@loan_amount`", Convert.ToDecimal(txtLDLAmount.Text.Trim()));
@@ -100,26 +126,16 @@ namespace MuslimAID.MURABHA
                     cmdInsert.Parameters.AddWithValue("`@period`", Convert.ToDecimal(cmbPeriod.SelectedValue.ToString()));
                     cmdInsert.Parameters.AddWithValue("`@monthly_instollment`", Convert.ToDecimal(txtLDMInstoll.Text.Trim()));
                     cmdInsert.Parameters.AddWithValue("`@reason_to_apply`", txtResonToApply.Text.Trim());
-
-                    if(rdoYes.Checked)
+                    if (rdoYes.Checked)
+                    {
                         cmdInsert.Parameters.AddWithValue("`@any_unsettled_loans`", 1);
+                        cmdInsert.Parameters.AddWithValue("`@other_unsettled_facilities`", checkOtherFacility());
+                    }
                     else cmdInsert.Parameters.AddWithValue("`@any_unsettled_loans`", 0);
-                    
-
-                    //cmdInsert.Parameters.AddWithValue("`@service_charges`", txtSupplier.Text.Trim());
-                    //cmdInsert.Parameters.AddWithValue("`@service_charges`", txtSupplierTelephone.Text.Trim());
-                    //cmdInsert.Parameters.AddWithValue("`@service_charges`", txtSupplierMobile.Text.Trim());
-                    //cmdInsert.Parameters.AddWithValue("`@service_charges`", txtSupplierAddress.Text.Trim());
-
-                    
-
-                    //cmdInsert.Parameters.AddWithValue("`@created_on`", Convert.ToDecimal(txtAccountName.Text.Trim()));
                     #endregion
-
                     try
                     {
                         int i = objDBCon.insertEditData(cmdInsert);
-
                         if (i > 0)
                         {
                             lblMsg.Text = "Successfull";
@@ -127,13 +143,57 @@ namespace MuslimAID.MURABHA
                     }
                     catch (Exception ex)
                     {
-
                     }
                 }
             }
             catch (Exception)
             {
             }
+        }
+
+        protected string checkOtherFacility() {
+            string strOtherFacility = "";
+            strOtherFacility = "[";
+            if (txtNameOrg1.Text.Trim() != "")
+            {
+                strOtherFacility += "{'organization' = '" + txtNameOrg1.Text.Trim() + "','purpose' = '" + txtPurpos1.Text.Trim() + "','facilityAmount' = '" + txtFAmount1.Text.Trim() + "','outstanding' = '" + txtOutstandBal1.Text.Trim() + "','m_installment' = '" + txtMonthInstal1.Text.Trim() + "','re_no_of_installments' = '" + txtRemainInstal1.Text.Trim() + "'}]";
+            }
+            if (txtNameOrg2.Text.Trim() != "")
+            {
+                strOtherFacility += ",{'organization' = '" + txtNameOrg2.Text.Trim() + "','purpose' = '" + txtPurpos2.Text.Trim() + "','facilityAmount' = '" + txtFAmount2.Text.Trim() + "','outstanding' = '" + txtOutstandBal2.Text.Trim() + "','m_installment' = '" + txtMonthInstal2.Text.Trim() + "','re_no_of_installments' = '" + txtRemainInstal2.Text.Trim() + "'}";
+            }
+            if (txtNameOrg3.Text.Trim() != "")
+            {
+                strOtherFacility += ",{'organization' = '" + txtNameOrg3.Text.Trim() + "','purpose' = '" + txtPurpos3.Text.Trim() + "','facilityAmount' = '" + txtFAmount3.Text.Trim() + "','outstanding' = '" + txtOutstandBal3.Text.Trim() + "','m_installment' = '" + txtMonthInstal3.Text.Trim() + "','re_no_of_installments' = '" + txtRemainInstal3.Text.Trim() + "'}";
+            }
+
+            strOtherFacility += "]";
+
+            return strOtherFacility.ToString();
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int strAnyOtherFacility;
+            if (rdoYes.Checked)
+            {
+                strAnyOtherFacility = 1;
+            }
+            else strAnyOtherFacility = 0;
+
+            string q = "UPDATE `muslimaid`.`micro_loan_details` SET loan_amount ='" + Convert.ToDecimal(txtLDLAmount.Text.Trim()) + "', product_category ='" + txtProdCate.Text.Trim() + "', brand ='" + txtBrand.Text.Trim() + "', model_no ='" + txtModelNo.Text.Trim() + "', selling_price ='" + txtSellPrice.Text.Trim() + "', down_payment ='" + txtDownPay.Text.Trim() + "', service_charges ='" + txtLDSerCharges.Text.Trim() + "', registration_fee = '" + txtRegistrationFee.Text.Trim() + "', walfare_fee ='" + txtWalfareFee.Text.Trim() + "', other_charges ='" + Convert.ToDecimal(txtLDOtherCharg.Text.Trim()) + "', interest_rate ='" + Convert.ToDecimal(txtLDIntRate.Text.Trim()) + "', period ='" + Convert.ToDecimal(cmbPeriod.SelectedValue.ToString()) + "', monthly_instollment ='" + Convert.ToDecimal(txtLDMInstoll.Text.Trim()) + "', reason_to_apply ='" + txtResonToApply.Text.Trim() + "', any_unsettled_loans ='" + strAnyOtherFacility + "', other_unsettled_facilities ='" + checkOtherFacility() + "' WHERE `contra_code` ='" + txtCC.Text.Trim() + "'";
+            try
+            {
+                int i = objDBCon.insertEditData(cmdInsert);
+                if (i > 0)
+                {
+                    lblMsg.Text = "Successfull";
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+
         }
     }
 }
