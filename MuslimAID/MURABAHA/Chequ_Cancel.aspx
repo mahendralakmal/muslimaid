3088e614645294f7e9e5b4377482025852e8ff23
mahen <gitlab.proit@gmail.com>
2016-12-22 03:13:29 +0800

    Added cheque cancelation form

---
+++ MuslimAID/MURABAHA/Chequ_Cancel.aspx
@@ -0,0 +1,68 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/MURABAHA/Murabha.Master" AutoEventWireup="true" CodeBehind="Chequ_Cancel.aspx.cs" Inherits="MuslimAID.MURABAHA.Chequ_Cancel" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="container">
+        <div class="PageTitle"><h4>Cheque Cancel</h4></div>
+        <div class="col-md-6 form-container">
+            <div class="form-control">
+                <div class="col-md-5">Cheque No</div>
+                <div class="col-md-7"><asp:TextBox ID="txtRNo" MaxLength="15" CssClass="form-control" AutoPostBack="true" runat="server" Width="110px" 
+                        TabIndex="0" ontextchanged="txtRNo_TextChanged" ></asp:TextBox></div>
+            </div>
+            <div class="form-control">
+                <div class="col-md-5">Contract Code</div>
+                <div class="col-md-7"><asp:Label ID="lblContractCode" runat="server"></asp:Label></div>
+            </div>
+            <div class="form-control">
+                <div class="col-md-5">NIC</div>
+                <div class="col-md-7"><asp:Label ID="lblNIC" runat="server"></asp:Label></div>
+            </div>
+            <div class="form-control">
+                <div class="col-md-5">Name</div>
+                <div class="col-md-7"><asp:Label ID="lblName" runat="server"></asp:Label></div>
+            </div>
+            <div class="form-control">
+                <div class="col-md-5">Amount</div>
+                <div class="col-md-7"><asp:Label ID="lblAmount" runat="server"></asp:Label></div>
+            </div>
+            <div class="form-control">
+                <div class="col-md-5">Cheque Date</div>
+                <div class="col-md-7"><asp:Label ID="lblChequeDate" runat="server"></asp:Label></div>
+            </div>
+            <div class="form-control">
+                <div class="col-md-5">Account No</div>
+                <div class="col-md-7"><asp:Label ID="lblAccountNo" runat="server"></asp:Label></div>
+            </div>
+            <div class="form-control">
+                <div class="col-md-5">Comment</div>
+                <div class="col-md-7">
+                    <asp:DropDownList ID="txtComment" CssClass="form-control" runat="server" AutoPostBack="True" 
+                        onselectedindexchanged="txtComment_SelectedIndexChanged">
+                        <asp:ListItem Value="1">Cheque amount is wrong</asp:ListItem>
+                        <asp:ListItem Value="2">Payee NIC is wrong</asp:ListItem>
+                        <asp:ListItem Value="3">Cheque payee is wrong </asp:ListItem>
+                        <asp:ListItem Value="4">Cheque date is wrong</asp:ListItem>
+                        <asp:ListItem Value="5">Cheque not crossed</asp:ListItem>
+                        <asp:ListItem Value="6">Cheque crossed</asp:ListItem>
+                        <asp:ListItem Value="7">Cheque number is wrong</asp:ListItem>
+                        <asp:ListItem Value="8">Other</asp:ListItem>
+                    </asp:DropDownList>
+                </div>
+            </div>
+            <div class="form-control">
+                <div class="col-md-5"></div>
+                <div class="col-md-7">
+                    <asp:TextBox ID="txtOther" MaxLength="45" AutoPostBack="true" runat="server" Width="165px" 
+                        TabIndex="4"  ></asp:TextBox></div>
+            </div>
+            <div class="form-control">
+                <div class="col-md-12">
+                    <asp:Button ID="btnPeied" runat="server" Enabled="false" TabIndex="3" 
+                        Text="Cancel" onclick="btnPeied_Click" />&nbsp;
+                    <asp:Label ID="lblMsg" runat="server"></asp:Label>
+                </div>
+            </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ MuslimAID/MURABAHA/Chequ_Cancel.aspx.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.Xml.Linq;
+using MySql.Data.MySqlClient;
+
+namespace MuslimAID.MURABAHA
+{
+    public partial class Chequ_Cancel : System.Web.UI.Page
+    {
+        cls_CommonFunctions objCommonTask = new cls_CommonFunctions();
+        cls_Connection objDBTask = new cls_Connection();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["LoggedIn"].ToString() == "True")
+            {
+                txtOther.Visible = false;
+            }
+            else
+            {
+                Response.Redirect("../Default.aspx");
+            }
+        }
+
+        protected void Clear()
+        {
+            lblAmount.Text = "";
+            lblContractCode.Text = "";
+            lblName.Text = "";
+            lblNIC.Text = "";
+            txtComment.Text = "";
+            lblChequeDate.Text = "";
+            lblAccountNo.Text = "";
+        }
+
+        private void ExistCheq()
+        {
+            try
+            {
+                lblMsg.Text = "";
+                Clear();
+                if (txtRNo.Text.Trim() != "")
+                {
+                    string strRNo = txtRNo.Text.Trim();
+                    DataSet dsChequeNo = cls_Connection.getDataSet("select * from chequebook_registry where cheq_no = '" + strRNo + "' ;");
+
+                    if (dsChequeNo.Tables[0].Rows.Count > 0)
+                    {
+                        if (dsChequeNo.Tables[0].Rows[0]["chq_status"].ToString() == "A")
+                        {
+                            DataSet dsCD = cls_Connection.getDataSet("select c.contract_code,c.nic,c.initial_name,h.amount AS paied_amount,concat('20',year1,year2,'-',month1,month2,'-',day1,day2) as ChequeDate from chq_date h inner join micro_basic_detail c on c.contract_code = h.contract_code inner join micro_loan_details l on l.contra_code = c.contract_code where l.chequ_no = '" + strRNo + "' and chq_status = 'A' and loan_sta != 'C';");
+                            if (dsCD.Tables[0].Rows.Count > 0)
+                            {
+                                lblAmount.Text = dsCD.Tables[0].Rows[0]["paied_amount"].ToString();
+                                lblContractCode.Text = dsCD.Tables[0].Rows[0]["contract_code"].ToString();
+                                lblNIC.Text = dsCD.Tables[0].Rows[0]["nic"].ToString();
+                                lblName.Text = dsCD.Tables[0].Rows[0]["initial_name"].ToString();
+                                lblChequeDate.Text = dsCD.Tables[0].Rows[0]["ChequeDate"].ToString();
+                                lblAccountNo.Text = dsChequeNo.Tables[0].Rows[0]["AccountNo"].ToString();
+                                btnPeied.Enabled = true;
+                            }
+                            else
+                            {
+                                lblMsg.Text = "No Record Found.";
+                                btnPeied.Enabled = false;
+                            }
+                        }
+                        else
+                        {
+                            lblMsg.Text = "This cheque number is already Cancel";
+                        }
+                    }
+                    else
+                    {
+                        lblMsg.Text = "No Record Found.";
+                    }
+                }
+                else
+                {
+                    lblMsg.Text = "Please enter Cheque No.";
+                    btnPeied.Enabled = false;
+                }
+            }
+            catch (Exception)
+            {
+                lblMsg.Text = "No Record Found.";
+            }
+        }
+
+        private void SaveCheqCancel()
+        {
+            try
+            {
+                lblMsg.Text = "";
+                if (txtRNo.Text.Trim() == "")
+                {
+                    lblMsg.Text = "Please enter Cheque No.";
+                }
+                else if (lblAmount.Text.Trim() == "")
+                {
+                    lblMsg.Text = "Please enter Amount.";
+                }
+                else if (lblContractCode.Text.Trim() == "")
+                {
+                    lblMsg.Text = "Please enter Contract Code.";
+                }
+                else if (lblName.Text.Trim() == "")
+                {
+                    lblMsg.Text = "Please enter Name.";
+                }
+                else if (lblNIC.Text.Trim() == "")
+                {
+                    lblNIC.Text = "Please enter NIC.";
+                }
+                else if (txtComment.Text.Trim() == "")
+                {
+                    lblMsg.Text = "Please enter Comment.";
+                }
+                else
+                {
+                    string strRNo = txtRNo.Text.Trim();
+                    string strCCode = lblContractCode.Text.Trim();
+                    string strAmount = lblAmount.Text.Trim();
+                    decimal decAmou = Convert.ToDecimal(strAmount);
+                    string strComment = txtComment.Text.Trim();
+                    if (txtComment.SelectedItem.Text == "Other")
+                    {
+                        strComment = txtOther.Text.Trim();
+                    }
+                    string strChequeDate = lblChequeDate.Text;
+                    string strDate = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+                    string strIp = Request.UserHostAddress;
+                    string strAccount = lblAccountNo.Text;
+                    string strloginID = Session["NIC"].ToString();
+
+                    MySqlCommand cmdUpdateCheque = new MySqlCommand("Update micro_loan_details set chequ_no = null,chequ_amount = 0.00,chequ_deta_on = '',cheq_detai_app_nic = '',due_date = '',maturity_date = '' where contra_code = '" + strCCode + "';");
+                    try
+                    {
+                        int f;
+                        f = objDBTask.insertEditData(cmdUpdateCheque);
+                        if (f == 1)
+                        {
+                            MySqlCommand cmdUpdateChequeNo = new MySqlCommand("Update chequebook_registry set chq_status = 'C', status = 0 where cheq_no = '" + strRNo + "';");
+                            try
+                            {
+                                int g;
+                                g = objDBTask.insertEditData(cmdUpdateChequeNo);
+                                if (g == 1)
+                                { }
+                            }
+                            catch (Exception ex)
+                            {
+                            }
+
+                            MySqlCommand cmdUpdateChe = new MySqlCommand("INSERT INTO chequebookCancel(`cheq_no`,`contract_code`,`chequ_deta_on`,`cancel_date`,`cancel_user`,`ChequeAmount`,`AccountNo`,Comment) values ('" + strRNo + "','" + strCCode + "','" + strChequeDate + "','" + strDate + "','" + strloginID + "','" + decAmou + "','" + strAccount + "','" + strComment + "');");
+                            try
+                            {
+                                int g;
+                                g = objDBTask.insertEditData(cmdUpdateChe);
+                                if (g == 1)
+                                { }
+                            }
+                            catch (Exception ex)
+                            {
+                            }
+
+                            MySqlCommand cmdUChequeNo = new MySqlCommand("Update chq_date set chq_status = 'C' where contract_code = '" + strCCode + "';");
+                            try
+                            {
+                                int g;
+                                g = objDBTask.insertEditData(cmdUChequeNo);
+                                if (g == 1)
+                                { }
+                            }
+                            catch (Exception ex)
+                            {
+                            }
+                            lblMsg.Text = "Cheque is Cancel.";
+                            btnPeied.Enabled = false;
+                            Clear();
+                            txtRNo.Text = "";
+                        }
+                        else
+                        {
+                            lblMsg.Text = "Error Occured!";
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        lblMsg.Text = "Error Occured!";
+                    }
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        protected void txtRNo_TextChanged(object sender, EventArgs e)
+        {
+            ExistCheq();
+        }
+
+        protected void btnPeied_Click(object sender, EventArgs e)
+        {
+            SaveCheqCancel();
+        }
+
+        protected void txtComment_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (txtComment.SelectedItem.Text == "Other")
+            {
+                txtOther.Visible = true;
+                txtOther.Text = "";
+                txtOther.Focus();
+            }
+            else
+            {
+                txtOther.Visible = false;
+                txtOther.Text = "";
+            }
+        }
+    }
+}
